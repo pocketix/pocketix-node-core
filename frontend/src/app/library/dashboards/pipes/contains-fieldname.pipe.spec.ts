e724cc25dc429d717f8d3bd45ccb3363b743ae41
Petr John <xjohnp00@stud.fit.vutbr.cz>
2022-05-06 11:18:46 +0200

    Pipe test

--- frontend/src/app/library/dashboards/pipes/contains-fieldname.pipe.spec.ts
+++ frontend/src/app/library/dashboards/pipes/contains-fieldname.pipe.spec.ts
@@ -1,8 +1,31 @@
 import { ContainsFieldNamePipe } from './contains-fieldname.pipe';
+import {DataItem} from "@swimlane/ngx-charts/lib/models/chart-data.model";
 
-describe('ContainsFieldnamePipe', () => {
+describe('ContainsFieldNamePipe', () => {
+  let pipe: ContainsFieldNamePipe;
+  const data = [{name: "name", value: 14}] as DataItem[];
+
+  beforeEach(() => {
+    pipe = new ContainsFieldNamePipe();
+  })
+
   it('create an instance', () => {
-    const pipe = new ContainsFieldNamePipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should find single perfect match', () => {
+    expect(pipe.transform({'name' : data}, 'name')).toEqual(data);
+  });
+
+  it('should find single', () => {
+    expect(pipe.transform({'stuff-name-stuff' : data}, 'name')).toEqual(data);
+  });
+
+  it('should not find', () => {
+    expect(pipe.transform({'name' : data}, 'should not find')).toEqual([]);
+  });
+
+  it('should find single in multiple', () => {
+    expect(pipe.transform({'name' : data, 'second' : []}, 'name')).toEqual(data);
   });
 });
--- frontend/src/app/library/dashboards/pipes/contains-fieldname.pipe.ts
+++ frontend/src/app/library/dashboards/pipes/contains-fieldname.pipe.ts
@@ -7,7 +7,8 @@ import {DataItem} from "@swimlane/ngx-charts/lib/models/chart-data.model";
 export class ContainsFieldNamePipe implements PipeTransform {
 
   transform(value: {[key: string]: DataItem[] }, name: string, ...args: unknown[]): DataItem[] {
-    const fieldData = Object.entries(value).find(([name, _]) => name.includes(name));
+    const fieldData = Object.entries(value).find(([valueName, _]) => valueName.includes(name));
+    console.log(fieldData, name);
     return fieldData ? fieldData[1] : [];
   }
 
